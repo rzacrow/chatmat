7ae0bc393b18f542a3b06c6bfc6a99016654bbdc
unknown <rezacrow88@gmail.com>
2023-10-28 14:18:02 +0330

    set configureation of mysqldb and modify urls of accounts app, and create urls.py,forms.py and define views in accounts app

--- chatmat/accounts/views.py
+++ chatmat/accounts/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.views.generic import View, FormView, CreateView, TemplateView
+from .forms import LoginForm
 
-# Create your views here.
+class Login(View):
+    def get(self, request):
+        form = LoginForm()
+        context = {
+            'form': form
+        }
+        
+        return render(request, 'login.html', context)
+    
+class Signup(View):
+    pass
+    
+class Logout(View):
+    pass
+
+class ForgetPassword(View):
+    pass
+
+class ResetPassword(View):
+    pass
+
+class Confirmation(View):
+    pass
